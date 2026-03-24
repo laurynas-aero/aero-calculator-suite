510fcd6200e931ff3f774703c772f9eb899970e0
Spiller362 <laurynasladd@yahoo.com>
2026-03-24 20:28:35 +0000

    Add docstrings to Mach_number/calculator.py

--- v2/logic/Mach_number/calculator.py
+++ v2/logic/Mach_number/calculator.py
@@ -1,9 +1,20 @@
-from logic.speed_of_sound.calculator import (
+from logic.Speed_of_sound.calculator import (
     compute_speed_of_sound_from_temperature, 
     compute_speed_of_sound_from_altitude
     )
 
 def compute_mach_number_from_temperature(temp, speed, units):
+    """
+    Computes Mach number from temperature and speed.
+
+    Parameters:
+        temp (float): Temperature (K or R depending on units)
+        speed (float): Speed (m/s or ft/s depending on units)
+        units (str): "si" or "english"
+
+    Returns:
+        float: Mach number (m/s or ft/s depending on units)
+        """
     speed_of_sound = compute_speed_of_sound_from_temperature(temp, units)
     _check_inputs(speed_of_sound, speed)
 
@@ -12,6 +23,17 @@ def compute_mach_number_from_temperature(temp, speed, units):
     return mach_number
 
 def compute_mach_number_from_altitude(alt, speed, units):
+    """
+    Computes Mach number from altitude and speed.
+
+    Parameters:
+        alt (float): Altitude (m or ft depending on units)
+        speed (float): Speed (m/s or ft/s depending on units)
+        units (str): "si" or "english"
+
+    Returns:
+        float: Mach number (m/s or ft/s depending on units)
+        """
     speed_of_sound = compute_speed_of_sound_from_altitude(alt, units)
     _check_inputs(speed_of_sound, speed)
 
@@ -20,6 +42,16 @@ def compute_mach_number_from_altitude(alt, speed, units):
     return mach_number
 
 def compute_mach_number_from_speed_of_sound(speed_of_sound, speed):
+    """
+    Computes Mach number from the speed of sound and speed.
+
+    Parameters:
+        speed_of_sound (float): Speed of sound (m/s or ft/s depending on units)
+        speed (float): Speed (m/s or ft/s depending on units)
+
+    Returns:
+        float: Mach number (m/s or ft/s depending on units)
+        """
     _check_inputs(speed_of_sound, speed)
     
     mach_number = speed/speed_of_sound
@@ -27,23 +59,65 @@ def compute_mach_number_from_speed_of_sound(speed_of_sound, speed):
     return mach_number
 
 def compute_speed_from_mach_number(mach_number, speed_of_sound):
+    """
+    Computes speed from the Mach number and speed of sound.
+
+    Parameters:
+        mach_number (float): Mach number
+        speed_of_sound (float): Speed of sound (m/s or ft/s depending on units)
+
+    Returns:
+        float: Speed (m/s or ft/s depending on units)
+        """
     speed = mach_number*speed_of_sound
 
     return speed
 
 def compute_speed_from_temperature(mach_number, temp, units):
+    """
+    Computes speed from the temperature and temperature.
+
+    Parameters:
+        mach_number (float): Mach number
+        temp (float): Temperature (K or R depending on units)
+        units (str): "si" or "english"
+
+    Returns:
+        float: Speed (m/s or ft/s depending on units)
+        """
     speed_of_sound = compute_speed_of_sound_from_temperature(temp, units)
     speed = mach_number*speed_of_sound
 
     return speed
 
 def compute_speed_from_altitude(mach_number, alt, units):
+    """
+    Computes speed from the Mach number and altitude.
+
+    Parameters:
+        mach_number (float): Mach number
+        alt (float): Altitude (m or ft depending on units)
+        units (str): "si" or "english"
+
+    Returns:
+        float: Speed (m/s or ft/s depending on units)
+        """
     speed_of_sound = compute_speed_of_sound_from_altitude(alt, units)
     speed = mach_number*speed_of_sound
 
     return speed
 
 def compute_speed_of_sound_from_mach_number(speed, mach_number):
+    """
+    Computes speed of sound from the speed and Mach number.
+
+    Parameters:
+        speed (float): Speed (m/s or ft/s depending on units)
+        mach_number (float): Mach number
+
+    Returns:
+        float: Speed of sound (m/s or ft/s depending on units)
+        """
     speed_of_sound = speed/mach_number
 
     return speed_of_sound
